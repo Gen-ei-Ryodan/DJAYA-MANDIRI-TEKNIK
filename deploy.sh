b50e5f2e2511ccde612e36fd7fd89fb6a87ca8db
Cristianalbertus <10969sosho@Cristianalbertuss-MacBook-Air.local>
2026-06-27 21:10:19 +0700

    chore: update deploy.sh paths to jayapetir.com

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cp -r css ~/public_html/petir.solusisurabaya.com/
-cp -r js ~/public_html/petir.solusisurabaya.com/
-cp -r laravel ~/public_html/petir.solusisurabaya.com/
+cp -r css ~/public_html/jayapetir.com/
+cp -r js ~/public_html/jayapetir.com/
+cp -r laravel ~/public_html/jayapetir.com/
 
-cp index.php ~/public_html/petir.solusisurabaya.com/
-cp .htaccess ~/public_html/petir.solusisurabaya.com/
-cp favicon.ico ~/public_html/petir.solusisurabaya.com/
-cp robots.txt ~/public_html/petir.solusisurabaya.com/
+cp index.php ~/public_html/jayapetir.com/
+cp .htaccess ~/public_html/jayapetir.com/
+cp favicon.ico ~/public_html/jayapetir.com/
+cp robots.txt ~/public_html/jayapetir.com/
