78e28b7276de174ef1e24dcaf3f03c1e5ba6b8c2
Cristianalbertus <10969sosho@Cristianalbertuss-MacBook-Air.local>
2026-06-30 12:24:43 +0700

    fix: add missing Textarea import in CityForm schema

--- app/Filament/Resources/Cities/Schemas/CityForm.php
+++ app/Filament/Resources/Cities/Schemas/CityForm.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\Cities\Schemas;
 
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
 use Filament\Schemas\Schema;
 
 class CityForm
